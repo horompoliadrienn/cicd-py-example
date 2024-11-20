83dbbad7cb9ab7a36773188e5d6bac4c1cc7e7c1
horompoliadrienn <azeletegyalomutbanahalalfele@gmail.com>
2024-11-20 12:43:50 +0100

    alahuzaskiturlese_.py

--- test_mymath.py
+++ test_mymath.py
@@ -11,6 +11,6 @@ def test_add_zero():
 def test_add_negative():
     assert add_numbers(4, -100) == -96
 
-def test_add_string__expect_exception():
+def test_add_string_expect_exception():
     with pytest.raises(TypeError):
         add_numbers(4, 'I DO NOT BELONG HERE')
